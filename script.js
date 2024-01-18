5b9e776bdc0bc643694a51960cd6b4edf859d512
Paritosh Parashar <parasharparitosh2003@gmail.com>
2024-01-18 23:36:20 +0100

    Setup initial objects

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const Gameboard = {
+
+   createBoard : () => {
+
+    r1 = Array(3).fill(-1);
+    r2 = Array(3).fill(-1);
+    r3 = Array(3).fill(-1);
+
+    return [r1,r2,r3];
+   },
+   
+};
+
+const Players = {
+
+    player1 : 'X',
+    player2 : 'O',
+
+};
+
+const Flow = {
+
+    
+
+}
