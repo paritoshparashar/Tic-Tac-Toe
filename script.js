c0fc2d0859fe2190a5a3cb461acb1b5d2e0f738f
Paritosh Parashar <parasharparitosh2003@gmail.com>
2024-01-23 03:41:50 +0100

    Add checkWin function to declare the winner, but it has a bug, still have to figure out how to deal with my function considering '_' symbol as the winning combination

--- script.js
+++ script.js
@@ -81,9 +81,66 @@ function GamePlay (){
         activePlayer = activePlayer === Players[0] ? Players[1] : Players[0];
     }
 
+    function checkWin(){
+
+        let current = boardObj.board;
+        let winStatus = {
+            WIN : false,
+            TIE : false,
+            WINSYMBOL : '_'
+        }
+
+        switch (true) {
+            case ((current[0][0].getSymbol() === current[0][1].getSymbol())=== current[0][2].getSymbol()):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[0][0].getSymbol();
+                break;
+
+            case ((current[1][0].getSymbol() === current[1][1].getSymbol())=== current[1][2].getSymbol()):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[1][0].getSymbol();
+                break;
+
+            case ((current[2][0].getSymbol() === current[2][1].getSymbol())=== (current[2][2].getSymbol() === current[2][0].getSymbol())):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[2][0].getSymbol();
+                break;
+
+            case ((current[0][0].getSymbol() === current[1][0].getSymbol())=== (current[2][0].getSymbol() === current[0][0].getSymbol())):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[0][0].getSymbol();
+                break;
+
+            case ((current[0][1].getSymbol() === current[1][1].getSymbol())=== (current[2][1].getSymbol() === current[0][1].getSymbol())):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[0][1].getSymbol();
+                break;
+
+            case ((current[0][2].getSymbol() === current[1][2].getSymbol())=== (current[2][2].getSymbol() === current[0][2].getSymbol())):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[0][2].getSymbol();
+                break;
+
+            case ((current[0][0].getSymbol() === current[1][1].getSymbol())=== (current[2][2].getSymbol() === current[0][0].getSymbol())):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[0][0].getSymbol();
+                break;
+
+            case ((current[2][0].getSymbol() === current[1][1].getSymbol())=== (current[0][2].getSymbol() === current[1][1].getSymbol())):
+                winStatus.WIN = true;
+                winStatus.WINSYMBOL = current[2][0].getSymbol();
+                break;
+
+            default:
+                break;
+        }
+
+        return winStatus
+        
+    }
     
 
-    function printBoard (){
+    function printBoard (status){
 
         for (let i = 0; i < 3; i++) {
 
@@ -99,7 +156,18 @@ function GamePlay (){
 
         }
 
-        console.log(`Player ${activePlayer.name}'s Turn...`)
+        if (status.WIN) {
+            
+            console.log(`${status.WINSYMBOL} wins!`);
+        }
+
+        else if (status.TIE){
+            console.log('It\'s a tie!')
+        }
+
+        else{
+            console.log(`Player ${activePlayer.name}'s Turn...`)
+        }
 
 
     }
@@ -113,19 +181,29 @@ function GamePlay (){
         if(moveSuccessfull){
 
             setActivePlayer();
-            printBoard();
+            let status = checkWin();
+            printBoard(status);
+            
+
         }
 
     }
 
     //Initial Setup message
-    printBoard();
+
+    //printBoard(status);
 
     return {
         playRound,
     }
 }
 
-
-    const game = GamePlay();
+function start (){
+        const game = GamePlay();
+        game.playRound(0,0);
+        game.playRound(0,1);
+        game.playRound(1,0);
+        game.playRound(2,2);
+        game.playRound(2,0);
+}
 
